082671bb762485736a1bb66562fadcf9f618ead8
VenomRei <74402173+VenomRei@users.noreply.github.com>
2023-10-06 12:08:41 -0500

    Add files via upload

---
+++ Code3_10016122606.c
@@ -0,0 +1,37 @@
+/* Jaden Veazia
+ * Code3_1001612606
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+#include "time.h"
+
+void fillBingoCard(int bingoCard[5][5])
+{
+    srand(time(NULL));
+    bingoCard[5][5] = rand();
+}
+
+
+
+void printBingoCard()
+{
+
+}
+
+int main()
+{
+    int bingoCard[5][5];
+    int bColumn[5];
+    int iColumn[5];
+    int nColumn[5];
+    int gColumn[5];
+    int oColumn[5];
+
+
+
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
